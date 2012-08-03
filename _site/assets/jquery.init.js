7ac2d709d0dc2b61c32e569b9fc92065262da0a3
metamn <bartus.csongor@gmail.com>
2012-08-03 11:31:08 +0300

    Starting to work on frontpage

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function() {
 
-  
+  // Rotate background images
+  jQuery(".startpage article img").each(function(index) {
+    jQuery(this).hide();
+    jQuery(this).delay(3000 * index).fadeIn(3000).fadeOut();
+  });  
   
 });
