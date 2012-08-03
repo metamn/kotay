f50d22b027179db33f4683adcf484efe8fc2a189
metamn <bartus.csongor@gmail.com>
2012-08-03 13:13:58 +0300

    Playing with frontpage navigation

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -6,4 +6,28 @@ jQuery(document).ready(function() {
     jQuery(this).delay(6000 * index).fadeOut('slow').fadeIn('slow');
   }); 
   
+  // Click on background image and show menu
+  jQuery("footer nav a").click(function(event) {
+    event.preventDefault();    
+    alert('a');
+        
+    jQuery('.arrow-right').animate({
+      borderLeftWidth: "70em",
+      borderTopWidth: "70em",
+      borderRightWidth: "70em",
+      borderBottomWidth: "70em",
+      top: "-80em"
+    }, 1500 );
+    
+    jQuery('.arrow-left').animate({
+      borderLeftWidth: "60em",
+      borderTopWidth: "60em",
+      borderRightWidth: "60em",
+      borderBottomWidth: "60em",
+      bottom: "-80em",
+      left: "35%"
+    }, 1500 );
+
+  });
+  
 });
